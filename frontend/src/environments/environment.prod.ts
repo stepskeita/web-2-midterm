e19901a34fc86fad6f998226674f636c2a888cc1
stepskeita <stepskeita@gmail.com>
2026-01-01 20:51:50 +0000

    fix: update apiUrl to production endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://web-2-midterm.onrender.com/api',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://web-2-midterm.onrender.com/api',
 };
